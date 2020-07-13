8938feb03e259b71dde2a95c978305e17f38002c
Gianluca <gianluca.piccardo@student.hu.nl>
2020-07-13 14:31:16 +0200

    Dynamic mesh created
    Mesh adressable from unity editor

---
+++ Isola/Assets/Scripts/Isola.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Isola : MonoBehaviour
+{
+    [Range(2, 256)]
+    public int resolution;
+    public bool autoUpdate = false;
+    private bool islandCreated = false;
+
+    GameObject isola;
+    MeshMaker meshMaker;
+
+    private void OnValidate()
+    {
+        BuildUp();
+        MakeNew();
+    }
+
+    public void BuildUp()
+    {
+        if (isola == null && islandCreated == false)
+        {
+            resolution = 10;
+            islandCreated = true;
+            isola = new GameObject("isola");
+            isola.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+            isola.AddComponent<MeshFilter>();
+            isola.GetComponent<MeshFilter>().sharedMesh = new Mesh();
+            meshMaker = new MeshMaker(isola.GetComponent<MeshFilter>().sharedMesh, resolution);
+        }
+        else
+        {
+            meshMaker = new MeshMaker(isola.GetComponent<MeshFilter>().sharedMesh, resolution);
+        }
+    }
+
+    void MakeNew()
+    {
+        meshMaker.MeshBuilder();
+    }
+}
---
+++ Isola/Assets/Scripts/MeshMaker.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshMaker
+{
+    Mesh mesh;
+    int resolution;
+
+    public MeshMaker(Mesh mesh, int resolution)
+    {
+        this.mesh = mesh;
+        this.resolution = resolution;
+    }
+
+    public void MeshBuilder()
+    {
+        Vector3[] vertices = new Vector3[(resolution + 1) * (resolution + 1)];
+        int[] triangles = new int[resolution * resolution * 6];
+        for(int i = 0, z = 0; z <= resolution; z++)
+        {
+            for(int x = 0; x <= resolution; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
+        int triangleIndex = 0;
+        int vertexIndex = 0;
+        for(int z = 0; z < resolution; z++)
+        {
+            for(int x = 0; x < resolution; x++)
+            {
+                triangles[triangleIndex + 0] = vertexIndex;
+                triangles[triangleIndex + 1] = vertexIndex + resolution + 1;
+                triangles[triangleIndex + 2] = vertexIndex + 1;
+
+                triangles[triangleIndex + 3] = vertexIndex + 1;
+                triangles[triangleIndex + 4] = vertexIndex + resolution + 1;
+                triangles[triangleIndex + 5] = vertexIndex + resolution + 2;
+                triangleIndex += 6;
+                vertexIndex++;
+            }
+            vertexIndex++;
+        }
+
+
+        
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
+
